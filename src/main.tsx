e931433a7e6f61a367d086d7e239bf2a018db9d3
KoT3uJlJla <48926752+KoT3uJlJla@users.noreply.github.com>
2026-05-18 13:11:07 +0500

    load v8 ui patch

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,10 @@ import { createRoot } from 'react-dom/client';
 import App from './App';
 import './styles.css';
 import './mobile-topbar.css';
+import './v8-ui-patch.css';
+import { installReleaseUiPatch } from './release-ui-patch';
+
+installReleaseUiPatch();
 
 createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
