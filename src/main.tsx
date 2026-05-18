41cbf034521dd88941a2a6cb89e88ffd024993e3
KoT3uJlJla <48926752+KoT3uJlJla@users.noreply.github.com>
2026-05-18 17:28:27 +0500

    install backend development ui patch

--- src/main.tsx
+++ src/main.tsx
@@ -5,8 +5,10 @@ import './styles.css';
 import './mobile-topbar.css';
 import './v8-ui-patch.css';
 import { installReleaseUiPatch } from './release-ui-patch';
+import { installBackendDevUiPatch } from './backend-dev-ui-patch';
 
 installReleaseUiPatch();
+installBackendDevUiPatch();
 
 createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
