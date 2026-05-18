3cf9edd5918aa9809716182bc8ce9f6099ae20ee
KoT3uJlJla <48926752+KoT3uJlJla@users.noreply.github.com>
2026-05-18 21:12:24 +0500

    install backend research ui patch

--- src/main.tsx
+++ src/main.tsx
@@ -8,6 +8,7 @@ import { installReleaseUiPatch } from './release-ui-patch';
 import { installBackendDevUiPatch } from './backend-dev-ui-patch';
 import { installBackendEconomyUiPatch } from './backend-economy-ui-patch';
 import { installAuthoritativeWalletUiPatch } from './authoritative-wallet-ui-patch';
+import { installBackendResearchUiPatch } from './backend-research-ui-patch';
 import { reconcileStartupSave } from './startup-save-reconcile';
 
 async function boot() {
@@ -17,6 +18,7 @@ async function boot() {
   installBackendDevUiPatch();
   installBackendEconomyUiPatch();
   installAuthoritativeWalletUiPatch();
+  installBackendResearchUiPatch();
 
   createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
