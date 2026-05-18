f6bf3f65a6c3016bc1f009d758f55a739ab5edaa
KoT3uJlJla <48926752+KoT3uJlJla@users.noreply.github.com>
2026-05-18 14:48:11 +0500

    install balance load fix

--- src/main.tsx
+++ src/main.tsx
@@ -5,8 +5,10 @@ import './styles.css';
 import './mobile-topbar.css';
 import './v8-ui-patch.css';
 import { installReleaseUiPatch } from './release-ui-patch';
+import { installBalanceLoadFix } from './balance-load-fix';
 
 installReleaseUiPatch();
+installBalanceLoadFix();
 
 createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
