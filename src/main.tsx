e47d1825f7e3279feae7f628d304f192aaadf3b0
KoT3uJlJla <48926752+KoT3uJlJla@users.noreply.github.com>
2026-05-19 11:07:32 +0500

    stabilize saves

--- src/main.tsx
+++ src/main.tsx
@@ -7,7 +7,6 @@ import './v8-ui-patch.css';
 import './predeploy-hardening.css';
 import './gameplay-ui-polish.css';
 import { installReleaseUiPatch } from './release-ui-patch';
-import { installAuthoritativeWalletUiPatch } from './authoritative-wallet-ui-patch';
 import { installGameplayUiPolish } from './gameplay-ui-polish';
 import { reconcileStartupSave } from './startup-save-reconcile';
 
@@ -15,7 +14,6 @@ async function boot() {
   await reconcileStartupSave();
 
   installReleaseUiPatch();
-  installAuthoritativeWalletUiPatch();
   installGameplayUiPolish();
 
   createRoot(document.getElementById('root')!).render(
