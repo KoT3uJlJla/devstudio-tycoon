7d7507beca3e2b81b531f1592d54fb61b06495db
KoT3uJlJla <48926752+KoT3uJlJla@users.noreply.github.com>
2026-05-24 23:24:29 +0500

    Remove obsolete post trust preload shim

--- server/post-trust-fix.js
+++
@@ -1,4 +0,0 @@
-import "./trustedRatings.js";
-
-// Compatibility shim: post-trust fixes are now part of clean trust modules
-// and index.js wiring. This file intentionally performs no runtime file patching.
